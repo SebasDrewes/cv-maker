7f9179970f327463e10c853c8c0114b04112febc
SebasDrewes <sebas.drewes@gmail.com>
2021-08-11 23:07:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  // eslint-disable-next-line react/jsx-filename-extension
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  // eslint-disable-next-line no-undef
+
   document.getElementById('root'),
 );
